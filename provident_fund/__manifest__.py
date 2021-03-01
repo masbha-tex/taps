00017c6fd4a60fb103bae7eb35801d984a707c32
fja-odoo <fja@odoo.com>
2021-03-01 10:55:04 +0800

    [ADD] provident_fund

---
+++ provident_fund/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Provident Fund",
+
+    'summary': "Aggragation of the Provident Fund contribution",
+
+    'author': "Odoo FJA",
+    'version': '14.0.1.0',
+    'depends': ['hr_payroll'],
+
+    'data': [
+        'views/hr_employee_views.xml',
+        'views/hr_salary_rule_views.xml',
+    ],
+}
---
+++ provident_fund/models/hr_salary_rule.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+class HrSalaryRule(models.Model):
+    _inherit = 'hr.salary.rule'
+
+    is_providient_found = fields.Boolean("Provident Fund")
