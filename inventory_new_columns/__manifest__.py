3adabd98f1775b5824e1e718519e2c2730b6bac1
nni-odoo <nni@odoo.com>
2021-05-26 16:21:42 +0800

    [ADD] inventory_new_columns

---
+++ inventory_new_columns/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Inventory New Columns",
+    # Explain the purpose of the module
+    "summary": """
+        Inventory New Columns scaffold module
+        """,
+    "category": "",
+    "version": "14.0.1.0.0",
+    "author": "Odoo PS",
+    "website": "http://www.odoo.com",
+    "license": "OEEL-1",
+    # Check depends order uncomment if necessary
+    "depends": [
+        'product',
+        'stock',
+        'stock_account',
+    ],
+    # Check data order
+    "data": [
+        "views/stock_quant.xml",
+    ],
+    # Only used to link to the analysis / Ps-tech store
+    "task_id": [2523902],
+}
