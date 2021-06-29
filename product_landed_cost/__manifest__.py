fd5a2769ebe077495d6e1c0ca9d9aeeacdb2998d
tau-odoo <tau@odoo.com>
2021-06-29 16:30:59 +0800

    [ADD] product_landed_cost
    Adding Total Landed Cost, Unit Value and Total Unit Value into
    product.product list view.
    
    [2557608]

---
+++ product_landed_cost/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+'name': 'product_landed_cost',
+'version': '14.0.1.0',
+'author': 'OdooHK-TAU',
+'depends': [
+    'stock_account',
+    'product',
+    ],
+'data': [
+    "views/product.xml",
+    ],
+}
