a7feacce3fb6a7df72a13bdd9b80e4d0a095965f
tau-odoo <tau@odoo.com>
2021-06-29 16:30:59 +0800

    [IMP] inventory_landed_cost
    Change search to read_group in _compute_landed_cost
    Adding Total Landed Cost, Unit Value and Total Unit Value into
    product.product list view.
    
    [2557608]

--- inventory_landed_cost/__manifest__.py
+++ inventory_landed_cost/__manifest__.py
@@ -2,21 +2,23 @@
     "name": "Inventory Landed Cost",
     # Explain the purpose of the module
     "summary": """
-        Addition of landed cost, unit value and total unit value under stock inventory report.
+        Addition of landed cost, unit value and total unit value under stock inventory report and product.product.
         """,
     "category": "",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2.0.0",
     "author": "Odoo PS",
     "website": "http://www.odoo.com",
     "license": "OEEL-1",
     # Check depends order uncomment if necessary
     "depends": [
         'stock_account',
+        'product',
     ],
     # Check data order
     "data": [
         "views/stock_quant.xml",
+        "views/product.xml",
     ],
     # Only used to link to the analysis / Ps-tech store
-    "task_id": [2523902],
+    "task_id": [2523902, 2557608],
 }
