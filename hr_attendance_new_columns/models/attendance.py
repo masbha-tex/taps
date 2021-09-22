e845972980765b32248550278f8bb58196d5f0ba
Abu Sayed <abu.sayed@texzipperbd.com>
2021-09-22 13:45:04 +0000

    add new columns in attendance view

---
+++ hr_attendance_new_columns/models/attendance.py
@@ -0,0 +1,25 @@
+#-*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class HrAttendance(models.Model):
+    _inherit = 'hr.attendance'
+#     _description = 'hr_attendance_new_columns.hr_attendance_new_columns'
+
+    date = fields.datetime.date(string = "Date", store=True)
+    inTime = fields.float(string = "In-Time")
+    inFlag = fields.char("In-Flag")
+    outTime = fields.float(string = "Out-Time")
+    outFlag = fields.char("Out-Flag")
+    otHours = fields.float(string = "Out-Time")
+     
+    
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
