92bfac39c67cf4d98528b89397b702f665ddec5b
adnan-tex <it@texzipperbd.com>
2021-09-27 16:47:20 +0000

    git

---
+++ po_report/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PO Report",
+
+    'summary': """
+        Group By Item Type""",
+
+    'description': """
+        PO Report grouping based on Item Type
+    """,
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "",
+    "version": "14.0.1.0.0",
+    "author": "Sayed",
+    "website": "http://www.odoo.com",
+    "license": "OEEL-1",
+    # Check depends order uncomment if necessary
+    "depends": [
+        'purchase',
+    ],
+    
+}
