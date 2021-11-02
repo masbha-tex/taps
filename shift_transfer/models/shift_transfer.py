a894e7934cf0b1e0128674d023dd5e21e8983498
masbha-tex <masbha.uddin@texzipperbd.com>
2021-11-02 15:17:14 +0000

    [Fix]

--- shift_transfer/models/shift_transfer.py
+++ shift_transfer/models/shift_transfer.py
@@ -6,10 +6,11 @@ class ShiftTransfer(models.Model):
     _name = 'shift.transfer'
     _description = 'Shift Transfer'
 
-    code = fields.Char(string='Code', store=True)
+    code = fields.Char(string='Code', readonly=True, store=True)
     name = fields.Many2one('hr.employee', string='Employee')
     empid = fields.Char(related= 'name.pin', related_sudo=False, string='Employee ID')
     activationDate = fields.Date(string='Activation Date')
     transferGroup = fields.Many2one('shift.setup', string='Shift Transfer Group')
     inTime = fields.Float(related= 'transferGroup.inTime', related_sudo=False, string='In-Time')
     outTime = fields.Float(related= 'transferGroup.outTime', related_sudo=False, string='Out-Time')
+    graceinTime = fields.Float(related= 'transferGroup.graceinTime', related_sudo=False, string="Grace In-Time")
