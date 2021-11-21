848a984bb5ca18cfccc47f2e09b2af14e6060880
adnan-tex <it@texzipperbd.com>
2021-11-21 07:03:22 +0000

    categ type

--- taps_inventory/models/product_template.py
+++ taps_inventory/models/product_template.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api
 
 class IncludeCateTypeInPT(models.Model):
     _inherit = 'product.template'
-    categ_type = fields.Many2one('category.type', 'Category Type', check_company=True)
+    categ_type = fields.Many2one('category.type', 'Category Type', check_company=True, change_default=True)
