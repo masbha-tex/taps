158ff00ceb9caaa7925d349cb68b2558834b6393
adnan-tex <it@texzipperbd.com>
2021-11-21 07:02:40 +0000

    modify B.report

--- stock_forecast_report_jsi/wizard/stock_forecast_report.py
+++ stock_forecast_report_jsi/wizard/stock_forecast_report.py
@@ -17,7 +17,7 @@ class StockForecastReport(models.TransientModel):
 
     report_by = fields.Selection([
         ('by_categories', 'By Categories'),
-        ('by_products', 'By Products')],
+        ('by_items', 'By Items')],
         default='by_categories')
     categ_ids = fields.Many2many('category.type', string='Categories')
     product_ids = fields.Many2many('product.product')
@@ -75,7 +75,7 @@ class StockForecastReport(models.TransientModel):
         to_date = self.to_date
         if not (self.product_ids or self.categ_ids):
             products = Product.search([('type', '=', 'product'),('default_code', 'like', 'R_')])
-        elif self.report_by == 'by_products':
+        elif self.report_by == 'by_items':
             products = self.product_ids
         else:
             products = Product.search([('categ_type', 'in', self.categ_ids.ids),('default_code', 'like', 'R_')])
@@ -86,6 +86,7 @@ class StockForecastReport(models.TransientModel):
         for categ in products.categ_type:
             #report_data.append([categ.display_name])
             categ_products = products.filtered(lambda x: x.categ_type == categ)
+            #stock_details = self.env['category.type'].search([('product_id', '=', productid),('schedule_date', '<', to_date)])
             report_product_data = []
             product_cat_data = []
             for product in categ_products:
@@ -115,6 +116,7 @@ class StockForecastReport(models.TransientModel):
 
                 product_data = [
                     '',
+                    '',
                     product.name,
                     opening_qty,
                     opening_value,
@@ -128,22 +130,26 @@ class StockForecastReport(models.TransientModel):
                 report_product_data.append(product_data)
             
             #sum(row[1] for row in report_product_data)
-            opening_categ_qty=sum(row[2] for row in report_product_data)#sum(report_product_data.mapped('opening_qty'))
-            opening_categ_value=sum(row[3] for row in report_product_data)
+            opening_categ_qty=sum(row[3] for row in report_product_data)#sum(report_product_data.mapped('opening_qty'))
+            opening_categ_value=sum(row[4] for row in report_product_data)
             #sum(report_product_data.mapped('opening_value'))
-            received_categ_qty=sum(row[4] for row in report_product_data)
+            received_categ_qty=sum(row[5] for row in report_product_data)
             #sum(report_product_data.mapped('received_qty'))
-            received_categ_value=sum(row[5] for row in report_product_data)
+            received_categ_value=sum(row[6] for row in report_product_data)
             #sum(report_product_data.mapped('received_value'))
-            issued_categ_qty=sum(row[6] for row in report_product_data)#sum(report_product_data.mapped('issued_qty'))
-            issued_categ_value=sum(row[7] for row in report_product_data)
+            issued_categ_qty=sum(row[7] for row in report_product_data)#sum(report_product_data.mapped('issued_qty'))
+            issued_categ_value=sum(row[8] for row in report_product_data)
             #sum(report_product_data.mapped('issued_value'))
-            closing_categ_qty=sum(row[8] for row in report_product_data)#sum(report_product_data.mapped('closing_qty'))
-            closing_categ_value=sum(row[9] for row in report_product_data)
+            closing_categ_qty=sum(row[9] for row in report_product_data)#sum(report_product_data.mapped('closing_qty'))
+            closing_categ_value=sum(row[10] for row in report_product_data)
             #sum(report_product_data.mapped('closing_value'))
             
+            parent_type = ''
+            if categ.parent_id.name:
+                parent_type = categ.parent_id.name
             product_cat_data = [
-                    categ.display_name,
+                    parent_type,
+                    categ.name,
                     '',
                     opening_categ_qty,
                     opening_categ_value,
@@ -156,7 +162,7 @@ class StockForecastReport(models.TransientModel):
                 ]
             
             report_data.append(product_cat_data)
-            if self.report_by == 'by_products':
+            if self.report_by == 'by_items':
                 for prodata in report_product_data:
                     report_data.append(prodata)
 
@@ -176,17 +182,20 @@ class StockForecastReport(models.TransientModel):
         row_categ_style = workbook.add_format({'bold': True, 'bg_color': '#6B8DE3'})
 
         # set the width od the column
-        worksheet.set_column(0, 9, 20)
-        worksheet.write(6, 0, 'Category', column_product_style)
-        worksheet.write(6, 1, 'Product', column_product_style)
-        worksheet.write(6, 2, 'Opening Quantity', column_product_style)
-        worksheet.write(6, 3, 'Opening Value', column_product_style)
-        worksheet.write(6, 4, 'Received Quantity', column_received_style)
-        worksheet.write(6, 5, 'Received Value', column_received_style)
-        worksheet.write(6, 6, 'Issued Quantity', column_issued_style)
-        worksheet.write(6, 7, 'Issued Value', column_issued_style)
-        worksheet.write(6, 8, 'Closing Quantity', column_product_style)
-        worksheet.write(6, 9, 'Closing Value', column_product_style)
+        
+        worksheet.set_column(0, 10, 20)
+        
+        worksheet.write(6, 0, 'Product', column_product_style)        
+        worksheet.write(6, 1, 'Category', column_product_style)
+        worksheet.write(6, 2, 'Item', column_product_style)
+        worksheet.write(6, 3, 'Opening Quantity', column_product_style)
+        worksheet.write(6, 4, 'Opening Value', column_product_style)
+        worksheet.write(6, 5, 'Received Quantity', column_received_style)
+        worksheet.write(6, 6, 'Received Value', column_received_style)
+        worksheet.write(6, 7, 'Issued Quantity', column_issued_style)
+        worksheet.write(6, 8, 'Issued Value', column_issued_style)
+        worksheet.write(6, 9, 'Closing Quantity', column_product_style)
+        worksheet.write(6, 10, 'Closing Value', column_product_style)
         col = 0
         row=7
         
@@ -194,7 +203,7 @@ class StockForecastReport(models.TransientModel):
             col=0
             categ=False
             for l in line:
-                if l != '' and col==0 :
+                if l != '' and col==1 :
                     categ=True
                 if categ==True:
                     worksheet.write(row, col, l, row_categ_style)
