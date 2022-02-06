d11a3f0966ee9fb4b9bb55b949bfe060473e68d1
masbha-tex <masbha.uddin@texzipperbd.com>
2022-02-06 07:15:21 +0000

    rm m

--- stock_forecast_report_jsi/wizard/stock_forecast_report.py
+++ stock_forecast_report_jsi/wizard/stock_forecast_report.py
@@ -83,12 +83,12 @@ class StockForecastReport(models.TransientModel):
         return val
     
     def getissue_qty(self,productid,from_date,to_date):
-        stock_details = self.env['stock.valuation.layer'].search([('product_id', '=', productid),('quantity', '<', 0),('schedule_date', '>=', from_date),('schedule_date', '<=', to_date),('description','not like','%Product Quantity Updated%')])
+        stock_details = self.env['stock.valuation.layer'].search([('product_id', '=', productid),('quantity', '<', 0),('schedule_date', '>=', from_date),('schedule_date', '<=', to_date)])#,('description','not like','%Product Quantity Updated%')
         qty = sum(stock_details.mapped('quantity'))
         return qty
     
     def getissue_val(self,productid,from_date,to_date):
-        stock_details = self.env['stock.valuation.layer'].search([('product_id', '=', productid),('value', '<', 0),('schedule_date', '>=', from_date),('schedule_date', '<=', to_date),('description','not like','%Product Quantity Updated%')])
+        stock_details = self.env['stock.valuation.layer'].search([('product_id', '=', productid),('value', '<', 0),('schedule_date', '>=', from_date),('schedule_date', '<=', to_date)])#,('description','not like','%Product Quantity Updated%')
         val = sum(stock_details.mapped('value'))
         return val
 
