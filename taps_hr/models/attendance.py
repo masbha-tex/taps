2167cc19da81b3dbf17aef2a370947453366b964
masbha-tex <masbha.uddin@texzipperbd.com>
2022-02-19 13:17:56 +0000

    Validation

--- taps_hr/models/attendance.py
+++ taps_hr/models/attendance.py
@@ -322,13 +322,13 @@ class HrAttendance(models.Model):
                 * maximum 1 "open" attendance record (without check_out)
                 * no overlapping time slices with previous employee records
         """
-        for attendance in self:
+        """for attendance in self:
             # we take the latest attendance before our check_in time and check it doesn't overlap with ours
             last_attendance_before_check_in = self.env['hr.attendance'].search([
                 ('employee_id', '=', attendance.employee_id.id),
                 ('check_in', '<=', attendance.check_in),
                 ('id', '!=', attendance.id),
-            ], order='check_in desc', limit=1)
+            ], order='check_in desc', limit=1)"""
             """if last_attendance_before_check_in and last_attendance_before_check_in.check_out and last_attendance_before_check_in.check_out > attendance.check_in:
                 raise exceptions.ValidationError(_("Cannot create new attendance record for %(empl_name)s, the employee was already checked in on %(datetime)s") % {
                     'empl_name': attendance.employee_id.name,
@@ -350,7 +350,7 @@ class HrAttendance(models.Model):
             else:"""
                 # we verify that the latest attendance with check_in time before our check_out time
                 # is the same as the one before our check_in time computed before, otherwise it overlaps
-            last_attendance_before_check_out = self.env['hr.attendance'].search([
+            """last_attendance_before_check_out = self.env['hr.attendance'].search([
                 ('employee_id', '=', attendance.employee_id.id),
                 ('check_in', '<', attendance.check_out),
                 ('id', '!=', attendance.id),
@@ -359,4 +359,4 @@ class HrAttendance(models.Model):
                 raise ValidationError(_("Cannot create new attendance record for %(empl_name)s, the employee was already checked in on %(datetime)s") % {
                     'empl_name': attendance.employee_id.name,
                     'datetime': format_datetime(self.env, last_attendance_before_check_out.check_in, dt_format=False),
-                })
+                })"""
