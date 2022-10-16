652844eafaea9e1dc1f5e1619dfb4e388a796d7e
adnan-tex <it@texzipperbd.com>
2022-10-16 10:32:02 +0000

    F

--- taps_hr/models/hr_appraisal_goal.py
+++ taps_hr/models/hr_appraisal_goal.py
@@ -90,148 +90,148 @@ class HrAppraisalGoal(models.Model):
         for app in self:
             if app.condition == 'less':
                 if int(app.weight)>=0 and int(app.target)>=0:
-                    if int(app.t_apr)>=0 and int(app.a_apr)>=0:
+                    if int(app.t_apr)>=0:
                         a = round(((app.a_apr and app.t_apr/app.a_apr)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < a or (int(app.t_apr) == 0 and int(app.a_apr) > 0) or (int(app.t_apr) == 0 and int(app.a_apr) == 0):
+                        if int(app.weight) < a or (int(app.t_apr) == 0 and int(app.a_apr) < 0) or (int(app.t_apr) == 0 and int(app.a_apr) == 0):
                             app.write({'apr': int(app.weight)})
                             
                         else:
                             app.write({'apr': a})
-                    if int(app.t_may)>=0 and int(app.a_may)>=0:
+                    if int(app.t_may)>=0:
                         m = round(((app.a_may and app.t_may/app.a_may)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < m or (int(app.t_may) == 0 and int(app.a_may) > 0) or (int(app.t_may) == 0 and int(app.a_may) == 0):
+                        if int(app.weight) < m or (int(app.t_may) == 0 and int(app.a_may) < 0) or (int(app.t_may) == 0 and int(app.a_may) == 0):
                             app.write({'may': int(app.weight)})
                         else:
                             app.write({'may': m})
-                    if int(app.t_jun)>=0 and int(app.a_jun)>=0:
+                    if int(app.t_jun)>=0:
                         j = round(((app.a_jun and app.t_jun/app.a_jun)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < j or (int(app.t_jun) == 0 and int(app.a_jun) > 0) or (int(app.t_jun) == 0 and int(app.a_jun) == 0):
+                        if int(app.weight) < j or (int(app.t_jun) == 0 and int(app.a_jun) < 0) or (int(app.t_jun) == 0 and int(app.a_jun) == 0):
                             app.write({'jun': int(app.weight)})
                         else:
                             app.write({'jun': j})
-                    if int(app.t_jul)>=0 and int(app.a_jul)>=0:
+                    if int(app.t_jul)>=0:
                         jl = round(((app.a_jul and app.t_jul/app.a_jul)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < jl or (int(app.t_jul) == 0 and int(app.a_jul) > 0) or (int(app.t_jul) == 0 and int(app.a_jul) == 0):
+                        if int(app.weight) < jl or (int(app.t_jul) == 0 and int(app.a_jul) < 0) or (int(app.t_jul) == 0 and int(app.a_jul) == 0):
                             app.write({'jul': int(app.weight)})
                         else:
                             app.write({'jul': jl})
-                    if int(app.t_aug)>=0 and int(app.a_aug)>=0:
+                    if int(app.t_aug)>=0:
                         au = round(((app.a_aug and app.t_aug/app.a_aug)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < au or (int(app.t_aug) == 0 and int(app.a_aug) > 0) or (int(app.t_aug) == 0 and int(app.a_aug) == 0):
+                        if int(app.weight) < au or (int(app.t_aug) == 0 and int(app.a_aug) < 0) or (int(app.t_aug) == 0 and int(app.a_aug) == 0):
                             app.write({'aug': int(app.weight)})
                         else:
                             app.write({'aug': au})
-                    if int(app.t_sep)>=0 and int(app.a_sep)>=0:
+                    if int(app.t_sep)>=0 :
                         se = round(((app.a_sep and app.t_sep/app.a_sep)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < se or (int(app.t_sep) == 0 and int(app.a_sep) > 0) or (int(app.t_sep) == 0 and int(app.a_sep) == 0):
+                        if int(app.weight) < se or (int(app.t_sep) == 0 and int(app.a_sep) < 0) or (int(app.t_sep) == 0 and int(app.a_sep) == 0):
                             app.write({'sep': int(app.weight)})
                         else:
                             app.write({'sep': se})
-                    if int(app.t_oct)>=0 and int(app.a_oct)>=0:
+                    if int(app.t_oct)>=0:
                         oc = round(((app.a_oct and app.t_oct/app.a_oct)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < oc or (int(app.t_oct) == 0 and int(app.a_oct) > 0) or (int(app.t_oct) == 0 and int(app.a_oct) == 0):
+                        if int(app.weight) < oc or (int(app.t_oct) == 0 and int(app.a_oct) < 0) or (int(app.t_oct) == 0 and int(app.a_oct) == 0):
                             app.write({'oct': int(app.weight)})
                         else:
                             app.write({'oct': oc})
-                    if int(app.t_nov)>=0 and int(app.a_nov)>=0:
+                    if int(app.t_nov)>=0:
                         no = round(((app.a_nov and app.t_nov/app.a_nov)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < no or (int(app.t_nov) == 0 and int(app.a_nov) > 0) or (int(app.t_nov) == 0 and int(app.a_nov) == 0):
+                        if int(app.weight) < no or (int(app.t_nov) == 0 and int(app.a_nov) < 0) or (int(app.t_nov) == 0 and int(app.a_nov) == 0):
                             app.write({'nov': int(app.weight)})
                         else:
                             app.write({'nov': no})
-                    if int(app.t_dec)>=0 and int(app.a_dec)>=0:
+                    if int(app.t_dec)>=0:
                         de = round(((app.a_dec and app.t_dec/app.a_dec)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < de or (int(app.t_dec) == 0 and int(app.a_dec) > 0) or (int(app.t_dec) == 0 and int(app.a_dec) == 0):
+                        if int(app.weight) < de or (int(app.t_dec) == 0 and int(app.a_dec) < 0) or (int(app.t_dec) == 0 and int(app.a_dec) == 0):
                             app.write({'dec': int(app.weight)})
                         else:
                             app.write({'dec': de})
-                    if int(app.t_jan)>=0 and int(app.a_jan)>=0:
+                    if int(app.t_jan)>=0:
                         ja = round(((app.a_jan and app.t_jan/app.a_jan)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < ja or (int(app.t_jan) == 0 and int(app.a_jan) > 0) or (int(app.t_jan) == 0 and int(app.a_jan) == 0):
+                        if int(app.weight) < ja or (int(app.t_jan) == 0 and int(app.a_jan) < 0) or (int(app.t_jan) == 0 and int(app.a_jan) == 0):
                             app.write({'jan': int(app.weight)})
                         else:
                             app.write({'jan': ja})
-                    if int(app.t_feb)>=0 and int(app.a_feb)>=0:
+                    if int(app.t_feb)>=0:
                         fe = round(((app.a_feb and app.t_feb/app.a_feb)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < fe or (int(app.t_feb) == 0 and int(app.a_feb) > 0) or (int(app.t_feb) == 0 and int(app.a_feb) == 0):
+                        if int(app.weight) < fe or (int(app.t_feb) == 0 and int(app.a_feb) < 0) or (int(app.t_feb) == 0 and int(app.a_feb) == 0):
                             app.write({'feb': int(app.weight)})
                         else:
                             app.write({'feb': fe})
-                    if int(app.t_mar)>=0 and int(app.a_mar)>=0:
+                    if int(app.t_mar)>=0:
                         ma = round(((app.a_mar and app.t_mar/app.a_mar)*int(app.weight))+0.01999,1)
-                        if int(app.weight) < ma or (int(app.t_mar) == 0 and int(app.a_mar) > 0) or (int(app.t_mar) == 0 and int(app.a_mar) == 0):
+                        if int(app.weight) < ma or (int(app.t_mar) == 0 and int(app.a_mar) < 0) or (int(app.t_mar) == 0 and int(app.a_mar) == 0):
                             app.write({'mar': int(app.weight)})
                         else:
                             app.write({'mar': ma})
             else:
                 if int(app.weight)>=0 and int(app.target)>=0:
-                    if int(app.t_apr)>=0 and int(app.a_apr)>=0:
+                    if int(app.t_apr)>=0:
                         a = round(((app.t_apr and app.a_apr/app.t_apr)*int(app.weight))+0.01999,1)
                         if int(app.weight) < a or (int(app.t_apr) == 0 and int(app.a_apr) > 0) or (int(app.t_apr) == 0 and int(app.a_apr) == 0):
                             app.write({'apr': int(app.weight)})
                         else:
                             app.write({'apr': a})
-                    if int(app.t_may)>=0 and int(app.a_may)>=0:
+                    if int(app.t_may)>=0:
                         m = round(((app.t_may and app.a_may/app.t_may)*int(app.weight))+0.01999,1)
                         if int(app.weight) < m or (int(app.t_may) == 0 and int(app.a_may) > 0) or (int(app.t_may) == 0 and int(app.a_may) == 0):
                             app.write({'may': int(app.weight)})
                         else:
                             app.write({'may': m})
-                    if int(app.t_jun)>=0 and int(app.a_jun)>=0:
+                    if int(app.t_jun)>=0:
                         j = round(((app.t_jun and app.a_jun/app.t_jun)*int(app.weight))+0.01999,1)
                         if int(app.weight) < j or (int(app.t_jun) == 0 and int(app.a_jun) > 0) or (int(app.t_jun) == 0 and int(app.a_jun) == 0):
                             app.write({'jun': int(app.weight)})
                         else:
                             app.write({'jun': j})
-                    if int(app.t_jul)>=0 and int(app.a_jul)>=0:
+                    if int(app.t_jul)>=0:
                         jl = round(((app.t_jul and app.a_jul/app.t_jul)*int(app.weight))+0.01999,1)
                         if int(app.weight) < jl or (int(app.t_jul) == 0 and int(app.a_jul) > 0) or (int(app.t_jul) == 0 and int(app.a_jul) == 0):
                             app.write({'jul': int(app.weight)})
                         else:
                             app.write({'jul': jl})
-                    if int(app.t_aug)>=0 and int(app.a_aug)>=0:
+                    if int(app.t_aug)>=0:
                         au = round(((app.t_aug and app.a_aug/app.t_aug)*int(app.weight))+0.01999,1)
                         if int(app.weight) < au or (int(app.t_aug) == 0 and int(app.a_aug) > 0) or (int(app.t_aug) == 0 and int(app.a_aug) == 0):
                             app.write({'aug': int(app.weight)})
                         else:
                             app.write({'aug': au})
-                    if int(app.t_sep)>=0 and int(app.a_sep)>=0:
+                    if int(app.t_sep)>=0:
                         se = round(((app.t_sep and app.a_sep/app.t_sep)*int(app.weight))+0.01999,1)
                         if int(app.weight) < se or (int(app.t_sep) == 0 and int(app.a_sep) > 0) or (int(app.t_sep) == 0 and int(app.a_sep) == 0):
                             app.write({'sep': int(app.weight)})
                         else:
                             app.write({'sep': se})
-                    if int(app.t_oct)>=0 and int(app.a_oct)>=0:
+                    if int(app.t_oct)>=0:
                         oc = round(((app.t_oct and app.a_oct/app.t_oct)*int(app.weight))+0.01999,1)
                         if int(app.weight) < oc or (int(app.t_oct) == 0 and int(app.a_oct) > 0) or (int(app.t_oct) == 0 and int(app.a_oct) == 0):
                             app.write({'oct': int(app.weight)})
                         else:
                             app.write({'oct': oc})
-                    if int(app.t_nov)>=0 and int(app.a_nov)>=0:
+                    if int(app.t_nov)>=0:
                         no = round(((app.t_nov and app.a_nov/app.t_nov)*int(app.weight))+0.01999,1)
                         if int(app.weight) < no or (int(app.t_nov) == 0 and int(app.a_nov) > 0) or (int(app.t_nov) == 0 and int(app.a_nov) == 0):
                             app.write({'nov': int(app.weight)})
                         else:
                             app.write({'nov': no})
-                    if int(app.t_dec)>=0 and int(app.a_dec)>=0:
+                    if int(app.t_dec)>=0:
                         de = round(((app.t_dec and app.a_dec/app.t_dec)*int(app.weight))+0.01999,1)
                         if int(app.weight) < de or (int(app.t_dec) == 0 and int(app.a_dec) > 0) or (int(app.t_dec) == 0 and int(app.a_dec) == 0):
                             app.write({'dec': int(app.weight)})
                         else:
                             app.write({'dec': de})
-                    if int(app.t_jan)>=0 and int(app.a_jan)>=0:
+                    if int(app.t_jan)>=0:
                         ja = round(((app.t_jan and app.a_jan/app.t_jan)*int(app.weight))+0.01999,1)
                         if int(app.weight) < ja or (int(app.t_jan) == 0 and int(app.a_jan) > 0) or (int(app.t_jan) == 0 and int(app.a_jan) == 0):
                             app.write({'jan': int(app.weight)})
                         else:
                             app.write({'jan': ja})
-                    if int(app.t_feb)>=0 and int(app.a_feb)>=0:
+                    if int(app.t_feb)>=0:
                         fe = round(((app.t_feb and app.a_feb/app.t_feb)*int(app.weight))+0.01999,1)
                         if int(app.weight) < fe or (int(app.t_feb) == 0 and int(app.a_feb) > 0) or (int(app.t_feb) == 0 and int(app.a_feb) == 0):
                             app.write({'feb': int(app.weight)})
                         else:
                             app.write({'feb': fe})
-                    if int(app.t_mar)>=0 and int(app.a_mar)>=0:
+                    if int(app.t_mar)>=0:
                         ma = round(((app.t_mar and app.a_mar/app.t_mar)*int(app.weight))+0.01999,1)
                         if int(app.weight) < ma or (int(app.t_mar) == 0 and int(app.a_mar) > 0) or (int(app.t_mar) == 0 and int(app.a_mar) == 0):
                             app.write({'mar': int(app.weight)})
