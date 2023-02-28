c4be8433c09549bc8105a87247cfaa4d2a0e0408
masbha-tex <masbha.uddin@texzipper.com>
2023-02-28 10:30:57 +0000

    con

--- taps_sale/models/sales.py
+++ taps_sale/models/sales.py
@@ -1156,8 +1156,9 @@ class SaleOrderLine(models.Model):
         else:
             size = values.get('sizein')
             
-        product_ = self.env['sale.order.line'].search([('product_id', '=', int(values.get('product_id')))])[1]
+        product_ = self.env['sale.order.line'].search([('product_id', '=', int(values.get('product_id')))],limit=1)
         #product_ = self.env['product.product'].search([('id', '=', pid)])
+        
         formula = self.env['fg.product.formula'].search([('product_tmpl_id', '=', product_.product_id.product_tmpl_id.id),('unit_type', '=', size_type)])
         wastage_tape = wastage_percent.search([('product_type', '=', formula.product_type),('material', '=', 'Tape')])
         wastage_slider = wastage_percent.search([('product_type', '=', formula.product_type),('material', '=', 'Slider')])
