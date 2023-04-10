f041a790765b05eca064dc2c30527fd77de39069
masbha-tex <masbha.uddin@texzipperbd.com>
2023-04-10 03:23:26 +0000

    all

--- taps_sale/__manifest__.py
+++ taps_sale/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '14.0.1.0.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','stock', 'web_studio','sales_team','web'],
+    'depends': ['base','sale','stock', 'web_studio','sales_team','web','report_xlsx'],
 
     # always loaded
     'data': [
