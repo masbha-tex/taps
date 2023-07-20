444fe160c7d900602f4fe3706d79866be06ae310
adnan-tex <66623105+adnan-tex@users.noreply.github.com>
2023-07-20 16:38:42 +0600

    Update __init__.py

--- taps_hr/wizard/__init__.py
+++ taps_hr/wizard/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from . import openacademy
+#from . import openacademy
 from . import employee_profile_report_wizard
 from . import salary_sheet_report_wizard
 from . import headwise_report_wizard
