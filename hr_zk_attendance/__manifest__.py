746cfd9bfe52a498dcc5324ede4172712d862467
adnan <it@texzipperbd.com>
2023-07-25 17:28:23 +0000

    FIX

--- hr_zk_attendance/__manifest__.py
+++ hr_zk_attendance/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Biometric Device Integration',
-    'version': '14.0.1.0.1',
+    'version': '14.0.1.0.0',
     'summary': "Integrating Biometric Device (Model: ZKteco uFace 202) With HR Attendance (Face + Thumb)",
     'description': "This module integrates Odoo with the biometric device(Model: ZKteco uFace 202),odoo13,odd,hr,attendance",
     'category': 'Generic Modules/Biometric',
@@ -20,5 +20,5 @@
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
-    'application': False,
+    'application': True,
 }
