c6f95f66c5524039bd289c4558a224fc8d891022
masbha-tex <masbha.uddin@texzipperbd.com>
2023-08-13 12:39:11 +0000

    A

--- taps_manufacturing/wizard/__init__.py
+++ taps_manufacturing/wizard/__init__.py
@@ -4,9 +4,5 @@ from . import mrp_split
 from . import bom_verification
 from . import mrp_plan
 from . import requisition
-<<<<<<< HEAD
-#from . import mrp_lot
-=======
 from . import mrp_lot
 from . import mrp_output
->>>>>>> fed867adae0ddf53ad8bbc77c460f81ea76674e4
