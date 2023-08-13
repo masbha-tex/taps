ce1b384c6a4e01c852a55e1027bcabc47ee63458
masbha-tex <66730742+masbha-tex@users.noreply.github.com>
2023-08-13 16:58:02 +0600

    Update __init__.py

--- taps_manufacturing/wizard/__init__.py
+++ taps_manufacturing/wizard/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from . import mrp_split
 #from . import manufacturing_report_wizard
-from . import bom_verification
-from . import mrp_plan
-from . import requisition
-from . import mrp_lot
-from . import mrp_output
+#from . import bom_verification
+#from . import mrp_plan
+#from . import requisition
+#from . import mrp_lot
+#from . import mrp_output
