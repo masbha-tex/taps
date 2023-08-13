5da2136706ffa34d2fbdef79dd712aceefcafda5
masbha-tex <masbha.uddin@texzipperbd.com>
2023-08-13 15:09:41 +0000

    Add

--- taps_retention_matrix/__manifest__.py
+++ taps_retention_matrix/__manifest__.py
@@ -12,10 +12,6 @@
 
     'author': "My Company",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Generic Modules/Retention Matrix',
     'version': '0.1',
 
