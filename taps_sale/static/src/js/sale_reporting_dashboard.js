b8cfa2d57383b348cd31e6f3b8e11d940c2131d4
masbha-tex <masbha.uddin@texzipperbd.com>
2023-10-29 16:48:55 +0000

    fix

--- taps_sale/static/src/js/sale_reporting_dashboard.js
+++ taps_sale/static/src/js/sale_reporting_dashboard.js
@@ -22,51 +22,17 @@ odoo.define('taps_sale.ReportingDashboard', function (require) {
         
         init: function () {
             this._super.apply(this, arguments);
-            this.dashboardValues = {};
+            
             
             
         
-        },
-        
-        
-        _loadDashboard: function (super_def) {
-        var self = this;
-        var dashboard_def = this._rpc({
-            model: 'sale.order',
-            method: 'retrieve_dashboard',
-            
-        });
-        return Promise.all([super_def, dashboard_def]).then(function(results) {
-            var id = results[0];
-            dashboardValues = results[1];
-            self.dashboardValues[id] = dashboardValues;
-            return id;
-        });
         }
         
         
-        // _render: function () {
-        // var self = this;
-        // return this._super.apply(this, arguments).then(function () {
-        //     var values = self.state.dashboardValues;
-        //     var sale_dashboard = QWeb.render('ReportingDashboard', {
-        //         values: values,
-        //     });
-            
-        //     // self.$el.parent().find(".o_sale_dashboard").remove();
-        //     self.$el.before(sale_dashboard);
-        // });
-        // },
-    //     _renderView: function () {
-    //     var self = this;
-    //     return this._super.apply(this, arguments).then(function () {
-    //         var values = self.state.dashboardValues;
-    //         var sale_dashboard = QWeb.render('ReportingDashboard', {
-    //             values: values,
-    //         });
-    //         self.$el.prepend(sale_dashboard);
-    //     });
-    // },
+        
+        
+        
+        
 
         
     });
