d1855f151a17baad206bfb1bcaebbaed587b898b
As-If13101245 <asif13101245@gmail.com>
2024-03-17 10:19:17 +0000

    Z1

--- business_excellence/__manifest__.py
+++ business_excellence/__manifest__.py
@@ -1,35 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "business_excellence",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'name': "Business Excellence",
+    'summary': """Business Excellence """,
+    'description': """Long description of Business Excellence module's""",
+    'author': "Mohammad Adnan",
+    'category': 'Generic Modules/Business Excellence',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','mail','web'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/business_excellence.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    
+    'qweb': [],    
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': True,
+    'images': ['static/src/img/icon.png'],
 }
